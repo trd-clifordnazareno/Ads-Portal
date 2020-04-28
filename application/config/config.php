13a15d7a762439f9b238a85027c452d1e6e8a52a
trd.clifordnazareno <trd.clifordnazareno@gmail.com>
2020-04-28 13:00:45 +0800

    new push

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://192.168.88.75/rmniportal';
+$config['base_url'] = 'richmedia.com.ph/rmniportal_sample/index.php/admin/home';
 
 /*
 |--------------------------------------------------------------------------
